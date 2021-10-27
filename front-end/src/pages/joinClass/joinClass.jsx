d769a4e788dcf94050a9844d9d1b84f72f1cae89
Tianshu Wang <tw2198@nyu.edu>
2021-10-27 13:18:04 -0400

    add mocked api into joinClass page

--- front-end/src/pages/joinClass/joinClass.jsx
+++ front-end/src/pages/joinClass/joinClass.jsx
@@ -2,29 +2,54 @@ import './joinClass.css'
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 
-export default function joinClass(props) {
+import { useState } from "react";
+import { useEffect } from "react";
+
+import axios from 'axios'
+
+const JoinClass = (props) => {
+    
+    const url = "https://617844e8bb979200171ff63e.mockapi.io/class/detail/"
+    const [course, setCourse] = useState(null)
+
+    useEffect(() =>{
+        let num = Math.floor(Math.random() * 49 + 1)
+        console.log(num)
+        axios.get(url + num).then((res) =>{
+            setCourse(res.data);
+        })
+    }, []);
+
+    const genClass= ()=>{
+        if(course != null){
+            return (
+                <div>
+                    <div className = "classTitle">
+                        <Avatar src = {course.avatar}>CL</Avatar>
+                        <div className = "classTitleInstructor">
+                            <div className = "className">{course.name}</div>
+                            <div className = "instructorName">Prof.{course.instructor}</div>
+                        </div>
+                    </div>
+                    <div className = "classDescription">
+                        <br />
+                        <div>
+                            {course.description}
+                        </div>
+                        <br />
+                    </div>
+                </div>
+            )
+        }
+    }
+
     return (
         <div className = "joinClass">
-            <div className = "classTitle">
-                <Avatar>CL</Avatar>
-                <div className = "classTitleInstructor">
-                    <div className = "className">Class Name</div>
-                    <div className = "instructorName">Prof.Foo Barstein</div>
-                </div>
-            </div>
-            <div className = "classDescription">
-                <br />
-                <div>
-                    Class Description: Lorem ipsum dolor sit amet, 
-                    consectetur adipiscing elit, sed do eiusmod tempor 
-                    incididunt ut labore et dolore magna aliqua. Ut enim 
-                    ad minim veniam
-                </div>
-                <br />
-            </div>
+            {genClass()}
             <div>
                 <Button variant="contained" className = "joinButton">Join Class</Button>
             </div>
         </div>
     )
 }
+export default JoinClass
