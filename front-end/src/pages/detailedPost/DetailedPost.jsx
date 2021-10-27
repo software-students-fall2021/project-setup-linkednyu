9dc694c57e6a70260901e7128099ef00f758e53e
AnastasiaYe <anastasia612@yahoo.com>
2021-10-27 13:59:05 -0400

    add fetching data

--- front-end/src/pages/detailedPost/DetailedPost.jsx
+++ front-end/src/pages/detailedPost/DetailedPost.jsx
@@ -2,10 +2,41 @@ import "./detailedPost.css"
 import { Button } from "../../components/Button"
 import Comment from "../../components/Comment"
 import ImageAvatars from "../../components/Avatar"
+import { useState, useEffect } from "react"
+import axios from 'axios'
 
 export default function DetailedPost() {
+    const url = "https://61798eeaaa7f340017404b69.mockapi.io/post"
+    const commenturl = "https://61798eeaaa7f340017404b69.mockapi.io/comment"
+    const [post, setPosts] = useState(null)
+    const [comment, setComments] = useState(null)
+    const [loading, setIsloading] = useState(true)
+    const [loadingComment, setIsloadingComment] = useState(true)
+
+    useEffect(() => {
+        async function fetchposts() {
+            await axios.get(url).then(response => {
+                setPosts(response.data[0])
+                setIsloading(false)
+            });
+
+        }
+        fetchposts()
+    }, [])
+
+    useEffect(() => {
+        async function fetchcomments() {
+            await axios.get(commenturl).then(response => {
+                setComments(response.data)
+                setIsloadingComment(false)
+            });
+
+        }
+        fetchcomments()
+    }, [])
+
     return (
-        <div className="detailedPost">
+        <> {!loading && !loadingComment && <div className="detailedPost" >
             <div className="detailedWrapper">
                 <div className="detailedPostTop">
                     <div className="detailedPostTopLeft">
@@ -15,11 +46,11 @@ export default function DetailedPost() {
                     </div>
                     <div className="detailedPostTopMiddle">
                         <div className="detailedPostTopMiddleTop">
-                            <div className="detailedPostClass">Software Engineering</div>
+                            <div className="detailedPostClass">{post.course_name}</div>
                         </div>
                         <div className="detailedPostTopMiddleBottom">
-                            <div className="detailedPostUserName">Anastasia Ye</div>
-                            <div className="detailedPosDate">5 mins ago</div>
+                            <div className="detailedPostUserName">{post.first_name} {post.last_name}</div>
+                            <div className="detailedPosDate">{post.date}</div>
                         </div>
                     </div>
                     <div className="detailedPostTopRight">
@@ -31,17 +62,17 @@ export default function DetailedPost() {
                     </div>
                 </div>
                 <div className="detailedPostCenter">
-                    <span className="detailedPostTitle">How to do HW3</span>
-                    <span className="detailedPostText">Helllllppppppp</span>
+                    <span className="detailedPostTitle">{post.title}</span>
+                    <span className="detailedPostText">{post.post}</span>
                     <img className="detailedPostImg" src="https://picsum.photos/200" alt="" />
                 </div>
                 <div className="detailedPostBottom">
-                    <span className="detailedPostCommentCounter">32 Comments</span>
+                    <span className="detailedPostCommentCounter">{comment.length} comments</span>
                 </div>
                 <div className="detailedPostComment">
-                    <Comment></Comment>
-                    <Comment></Comment>
-                    <Comment></Comment>
+                    {comment.map((p) => (
+                        <Comment comment={p} />
+                    ))}
                 </div>
                 <div className="detailedPostFooter">
                     <input placeholder="Comment Something..." className="detailedPostAddComment" />
@@ -52,6 +83,7 @@ export default function DetailedPost() {
                     > Comment  </Button>
                 </div>
             </div>
-        </div>
+        </div >}
+        </>
     )
 }
