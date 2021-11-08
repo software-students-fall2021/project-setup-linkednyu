71508a6a9436738a9abbfecc5ecfd61d13b96640
Tianshu Wang <tw2198@nyu.edu>
2021-11-08 10:39:18 -0500

    join/leave channel & channel detail

--- back-end/routes/userRoutes.js
+++ back-end/routes/userRoutes.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const routeFunctions = require("../controllers/routeFunctions");
+const channelFunctions = require("../controllers/channelDetail");
 
 
 // routes
 router.get('/homeposts', routeFunctions.viewHome)
 router.get('/channel/:id', routeFunctions.viewChannel)
+router.post('/channel/detail/:id', channelFunctions.channel)
+router.post('/channel/join/:id', channelFunctions.joinChannel)
+router.post('/channel/leave/:id', channelFunctions.leaveChannel)
 //should add an id
 router.get("/detailedposts/:id", routeFunctions.viewHome)
 router.get('/comments', routeFunctions.viewComment)
