6e1c4ccbe0718113135bf56675a500d3e81b143a
Tianshu Wang <tw2198@nyu.edu>
2021-11-10 12:43:19 -0500

    unit test setup

---
+++ back-end/test/channelDetail.spec.js
@@ -0,0 +1,20 @@
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+const app = require('../app')
+chai.use(chaiHttp)
+const assert = require('assert')
+
+//channel detail test
+describe('channel detail function test', ()=>{
+
+});
+
+// join channel test
+describe('join channel test', () => {
+
+});
+
+// leave channel test
+describe('leave channel test', () =>{
+
+});
