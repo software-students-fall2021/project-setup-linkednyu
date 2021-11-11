c947dd527b8cee3f130e512b2a29d0966a2e8b56
Tianshu Wang <tw2198@nyu.edu>
2021-11-11 14:54:49 -0500

    finished test

--- back-end/test/channelDetail.spec.js
+++ back-end/test/channelDetail.spec.js
@@ -1,20 +1,61 @@
 const chai = require('chai')
 const chaiHttp = require('chai-http')
 const app = require('../app')
-chai.use(chaiHttp)
+chai.should()
 const assert = require('assert')
 
 //channel detail test
 describe('channel detail function test', ()=>{
+    it('Channel detail', (done)=> {
+        for(let i = 1; i < 5; i++){
+            chai.request(app)
+            .post('/channel/detail/' + i)
+            .send("abc")
+            .end((err, res) =>{
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                res.body.should.have.property('id');
+                res.body.should.have.property('icon');
+                res.body.should.have.property('avg_grade');
+                res.body.should.have.property('rating');
+                res.body.should.have.property('detail');
+                res.body.should.have.property('enrolled');
+            })
+        }
+        done();
+    }).timeout(10000);
 
-});
+    it('Channel join/leave function', (done)=> {
+        chai.request(app)
+            .post('/channel/join/1')
+            .send("abc")
+            .end((err, res) =>{
+                res.should.have.status(200);
+            })
 
-// join channel test
-describe('join channel test', () => {
+            chai.request(app)
+            .post('/channel/detail/1')
+            .send("abc")
+            .end((err, res) =>{
+                res.should.have.status(200);
+                res.body.should.have.property('enrolled').eq(true);
+            })
 
-});
+        chai.request(app)
+            .post('/channel/leave/1')
+            .send("abc")
+            .end((err, res) =>{
+                res.should.have.status(200);
+            })
 
-// leave channel test
-describe('leave channel test', () =>{
+        chai.request(app)
+            .post('/channel/detail/1')
+            .send("abc")
+            .end((err, res) =>{
+                res.should.have.status(200);
+                res.body.should.have.property('enrolled').eq(false);
+            })
 
+            done();
+    })
 });
