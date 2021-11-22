a1f0c48f0a7c85c86f0f5647df2b94b7992ee27f
AnastasiaYe <anastasia612@yahoo.com>
2021-11-22 13:43:42 -0500

    fixed error

--- back-end/server.js
+++ back-end/server.js
@@ -14,19 +14,13 @@ mongoose.connect(process.env.CONNECTION_URL, { useNewURLParser: true, useUnified
   }))
   .catch((error) => console.log(error.message))
 
+// const CONNECTION_URL = 'mongodb+srv://linkednyu:nyu1234@linkednyu.3tdal.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+// mongoose.connect(CONNECTION_URL, { useNewURLParser: true, useUnifiedTopology: true })
+//   .then(() => server.listen(port, function () {
+//     console.log(`Server running on port: ${port}`)
+//   }))
+//   .catch((error) => console.log(error.message))
 
-// call a function to start listening to the port
-// const listener = server.listen(port, function () {
-//   console.log(`Server running on port: ${port}`)
-// })
-
-const CONNECTION_URL = 'mongodb+srv://linkednyu:nyu1234@linkednyu.3tdal.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true }) //connect project to atlas and set some default settings
-        .then(listener)
-        .catch((err) => {
-            console.log(err.message)
-        })
 
 // a function to stop listening to the port
 const close = () => {
