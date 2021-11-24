cb17fbfc21ac0dec0451617f29ebb8756fdbb979
Tianshu Wang <tw2198@nyu.edu>
2021-11-24 04:27:10 -0500

    joinClass db integration

--- front-end/src/pages/joinClass/joinClass.jsx
+++ front-end/src/pages/joinClass/joinClass.jsx
@@ -12,46 +12,98 @@ import { Link } from "react-router-dom"
 
 const JoinClass = (props) => {
     
-    const url = "https://617844e8bb979200171ff63e.mockapi.io/class/detail/"
+    const url = "/channel/"
     const [course, setCourse] = useState(null)
+    const [isJoined, setIsJoined] = useState(false);
+
+    let classId = props.match.params.id;
+    let postObj = {
+        email:"tw2198@nyu.edu",
+        channelId: classId
+    }
 
     useEffect(() =>{
-        let num = Math.floor(Math.random() * 49 + 1)
-        console.log(num)
-        axios.get(url + num).then((res) =>{
-            setCourse(res.data);
-        })
+        //get class info
+        axios.get(url + "detail/" +classId)
+            .then((res) =>{
+                if(res.data.length >= 0){
+                    setCourse(res.data[0]);
+                    axios.post(url + "isJoined/", postObj)
+                        .then((res1) =>{
+                            setIsJoined(res1.data.joined);
+                        })  
+                }
+            })
+
     }, []);
 
     const genClass= ()=>{
-        if(course != null){
+        if(course){
             return (
                 <div>
                     <div className = "classTitle">
-                        <Avatar src = {course.avatar}>CL</Avatar>
+                        <Avatar src = {course.icon}></Avatar>
                         <div className = "classTitleInstructor">
                             <div className = "className">{course.name}</div>
-                            <div className = "instructorName">Prof.{course.instructor}</div>
+                            <div className = "instructorName">{course.instructor}</div>
                         </div>
                     </div>
                     <div className = "classDescription">
                         <br />
                         <div>
-                            {course.description}
+                            {course.detail}
                         </div>
                         <br />
                     </div>
                 </div>
             )
+        }else{
+            return (
+                <div>
+                    <br />
+                    <div className = "classDescription">Sorry, We can't find the information about this course</div>
+                    <br />
+                </div>
+            )
         }
+    }
+
+    const joinClass = () => {
+        console.log("join class button click")
+        axios.post(url + "join/", postObj)
+                        .then((res) =>{
+                            axios.post(url + "isJoined/", postObj)
+                                .then((res1) =>{
+                                setIsJoined(res1.data.joined);
+                            })
+                        })    
+    }
+
+    const leaveClass = () => {
+        console.log("leave class button click")
+        axios.post(url + "leave/", postObj)
+                        .then((res) =>{
+                            axios.post(url + "isJoined/", postObj)
+                                .then((res1) =>{
+                                setIsJoined(res1.data.joined);
+                            })
+                        })   
+    }
+
+    const genButton = ()=>{
+        return (
+            <div>
+                {course && isJoined && <Button variant="contained" className = "joinButton" onClick={leaveClass}>Leave</Button>}
+                {course && !isJoined && <Button variant="contained" className = "joinButton" onClick={joinClass}>Join</Button>}
+                {!course && <Link to="/"><Button variant="contained" className = "joinButton">Return Home</Button></Link>}                
+            </div>
+        )
     }
 
     return (
         <div className = "joinClass">
             {genClass()}
-            <div>
-                <Link to="/channel/Mathematics"><Button variant="contained" className = "joinButton">Join Class</Button></Link>
-            </div>
+            {genButton()}
         </div>
     )
 }
