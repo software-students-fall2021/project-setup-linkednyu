154b94a0edbe7a18500e588fe128e1d3a71b8a41
princeampofo <pla252@nyu.edu>
2021-12-06 10:16:52 -0500

    deployment

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#replace this with the path of your project on the VPS
+cd ~/mernapp
+
+#pull from the branch
+git pull origin main
+
+# followed by instructions specific to your project that you used to do manually
+make up
